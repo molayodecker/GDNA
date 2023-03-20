20780d768e59c5a1dc85aa3bc6a774e9ad639d79
Molayo Decker <molayodecker@Mac-mini.local>
2023-03-19 22:12:08 -0400

    setting up repo

---
+++ inc/gdna_wp_nav_menu_walker.php
@@ -0,0 +1,17 @@
+<?php
+class gdna_wp_nav_menu_walker extends Walker_Nav_Menu
+{
+
+    function start_lvl(&$output, $depth = 0, $args = null)
+    {
+    }
+
+    function start_el(&$output, $item, $depth = 0, $args = null, $id = 0)
+    {
+    }
+
+    function end_el(&$output, $item, $depth = 0, $args = null)
+    {
+    }
+}
+?>
--- src/js/script.js
+++ src/js/script.js
@@ -10,9 +10,26 @@ function navToggle() {
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 
-  if (menu.classList.contains("flex")) {
-    logo.setAttribute("src", "./asset/logo/favicon.ico");
-  } else {
-    logo.setAttribute("src", "./asset/images/g-dna-logo-default.webp");
-  }
+  //   if (menu.classList.contains("flex")) {
+  //     logo.setAttribute("src", "./asset/logo/favicon.ico");
+  //   } else {
+  //     logo.setAttribute("src", "./asset/images/g-dna-logo-default.webp");
+  //   }
 }
+
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+
+    dropdown.classList.remove("active");
+  });
+});
